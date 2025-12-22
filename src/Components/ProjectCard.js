dde3563ea3f1f63b7e59c7054f84001bdf085537
Chawin <chawinmin@gmail.com>
2025-12-21 22:53:48 -0800

    Added animations for the project cards, fixed the hamburger button, and fixed bugs with mobile view on all work and home page

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function ProjectCard({
     miniTitle,
         badge,
@@ -9,9 +11,96 @@ export default function ProjectCard({
         projectFileName = "View Project"}
         ) 
     {
+    
+    //Reference to THIS project card DOM element
+    const cardRef = useRef(null);
+    const imageRef = useRef(null);
 
+    //Control the hover state for the card and the image
+    const [isHoveredCard, setIsHoveredCard] = useState(false);  
+    const [isHoveredImage, setIsHoveredImage] = useState(false);
+    
+    //Scrolling Animation
+    const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) entry.target.classList.add('show');
+        else entry.target.classList.remove('show');
+    });
+    }, 
+    {
+        threshold: 0.15,
+        rootMargin: "0px 0px -10% 0px",
+    })
+    const projectCardElements = document.querySelectorAll(".project-card");
+    projectCardElements.forEach(el => observer.observe(el));
+
+    requestAnimationFrame( () => {
+        const firstCard = document.querySelector(".project-card");
+        if (firstCard) firstCard.classList.add('show');
+    });
+
+    useEffect(() => {
+        //Store last known mouse position
+        let lastX = 0;
+        let lastY = 0;
+
+        //Preent excessive work during fast scrolling
+        let raf = null;
+
+        //Run whenever the mouse moves
+        const onMouseMove = (e) => {
+            lastX = e.clientX;
+            lastY = e.clientY;
+        };
+
+        //Check what element is under the cursor
+        const updateHoverFromPoint = () => {
+            const el = document.elementFromPoint(lastX, lastY);
+            const card = el?.closest?.(".project-card");
+            const image = el?.closest?.(".project-img");
+
+            //Is this card the onder under the cursor
+            const isThisCard = !!card && cardRef.current && card === cardRef.current;
+            setIsHoveredCard(isThisCard);
+
+            const isThisImage = !!image && imageRef.current && image === imageRef.current;
+            setIsHoveredImage(isThisImage);
+        };
+
+        //Runs when scrolling happens
+        const onScroll = () => {
+
+            //Prevnets multiple updates per frame
+            if(raf) return;
+            raf = requestAnimationFrame(() => {
+                raf = null;
+                updateHoverFromPoint();
+            });
+        };
+
+        //Listen for mouse movement
+        window.addEventListener("mousemove", onMouseMove, {passive: true});
+
+        //Listen for scrolling
+        window.addEventListener("scroll", onScroll,{passive: true});
+
+        //Check if cursor is over a card
+        updateHoverFromPoint();
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove);
+            window.removeEventListener("scroll", onScroll);
+            if(raf) cancelAnimationFrame(raf);
+        };
+    }, []);
+    
     return (
-        <article className = "project-card"> 
+        <article 
+            ref = {cardRef}
+            className={`project-card ${isHoveredCard ? "is-hovered" : ""}`}
+            onPointerEnter={() => setIsHoveredCard(true)}
+            onPointerLeave={() => {setIsHoveredCard(false); setIsHoveredImage(false);}}
+        >
             <div className = "project-left">
                 <div className = "project-top">
                     <span className = "project-mini">{miniTitle}</span>
@@ -29,12 +118,16 @@ export default function ProjectCard({
                 </div>
 
                 <a className="project-link" href={link}>
-                    View Project <span className="arrow">→</span>
+                    {projectFileName} <span className="arrow">→</span>
                 </a>
             </div>
 
             <div className = "project-right">
-                <img className="project-img" src={img} alt={title}/>
+                <img
+                ref = {imageRef} 
+                className= {`project-img ${isHoveredImage ? "is-hovered" : ""}`} 
+                src={img} 
+                alt={title}/>
             </div>
         </article>
     )
